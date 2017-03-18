de6f853e1dfc6e88dac99d278dfac403afb4b29e
Ivailo Karamanolev <ivailo@karamanolev.com>
2017-03-18 21:43:36 +0200

    Testing django app

---
+++ home/views.py
@@ -0,0 +1,22 @@
+import os
+import random
+import time
+
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+
+def index(request):
+    env = dict(os.environ)
+    return render(request, 'index.html', {
+        'env': env,
+    })
+
+
+@require_POST
+@csrf_exempt
+def complicated(request):
+    time.sleep(0.5 + random.random() * 0.5)
+    return HttpResponse('ok')
